47946dd591396b40e1b8d3cff3964af769a22b58
Zenzamy Othmane <ozenzamy.spark@gmail.com>
2022-01-27 19:34:10 +0100

    Change the env variable process with a Variable that contain the URL

--- src/components/body/features/Features.js
+++ src/components/body/features/Features.js
@@ -3,23 +3,24 @@ import './Responsive.css';
 
 
 function Features() {
+    var Path = process.env.PUBLIC_URL;
     return(
         <section className='Features'>
             <div className='one'>
                 <div className='livetv'>
-                    <img src={process.env.PUBLIC_URL+"/assets/img/livetv.jpg"} alt='Live TV'/>
+                    <img src={Path+"/assets/img/livetv.jpg"} alt='Live TV'/>
                     <h2>Live TV</h2>
                     <p>Des milliers de chaînes de télévision en direct dans le monde entier Premium HD et FHD Sports.</p>
                     <a href="#" className='readmorebtn'>Lire la suite...</a>
                 </div>
                 <div className='movies'>
-                    <img src={process.env.PUBLIC_URL+"/assets/img/movies.jpg"} alt='Live TV'/>
+                    <img src={Path+"/assets/img/movies.jpg"} alt='Live TV'/>
                     <h2>Movies</h2>
                     <p>Films fréquemment mis à jour, films de haute qualité, différentes catégories !</p>
                     <a href="#" className='readmorebtn'>Lire la suite...</a>
                 </div>
                 <div className='tvshows'>
-                    <img src={process.env.PUBLIC_URL+"/assets/img/tvshows.jpg"} alt='Live TV'/>
+                    <img src={Path+"/assets/img/tvshows.jpg"} alt='Live TV'/>
                     <h2>TV Shows</h2>
                     <p>Meilleures émissions de télévision de tous les temps Mises à jour fréquemment Séries télévisées Netflix-Hbo-Amazon !</p>
                     <a href="#" className='readmorebtn'>Lire la suite...</a>
