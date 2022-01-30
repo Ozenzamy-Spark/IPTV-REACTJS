dd6f33d039f28cc641de46af95ecee104c5ad0b9
Zenzamy Othmane <ozenzamy.spark@gmail.com>
2022-01-30 20:31:36 +0100

    Changing img alt attribute

--- src/components/body/channels/Channels.js
+++ src/components/body/channels/Channels.js
@@ -5,7 +5,7 @@ import './Channels.css';
 function Channels() {
     return (
         <section className='Channels' id="channels">
-            <center><img src={process.env.PUBLIC_URL + '/assets/img/channelList.webp'}/></center>
+            <center><img src={process.env.PUBLIC_URL + '/assets/img/channelList.webp'} alt='Channels list'/></center>
         </section>
     );
 }
